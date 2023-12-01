1d4dfddb35d621b1676980b6874641c2f242e9ff
Julien Midedji <julien.midedji@gmail.com>
2023-12-01 17:11:19 +0100

    Fix part two for real now. Damn

--- 2023/1_trebuchet/script2.sh
+++ 2023/1_trebuchet/script2.sh
@@ -1,62 +1,19 @@
 #!/bin/bash
 
-translate() {
-    result=$1
-
-    # We cannot substitute everything at once.
-    # E.g. "eightwothree" has multiple solutions depending on what is substituted first.
-    # So use a sliding window.
-    for (( i=0; i<${#1}; i++ )); do
-        # Check three digit numbers.
-        three=${1:$i:3}
-        case $three in
-            "one") 
-                result=$(echo $result | sed "s/one/1/")
-                ;;
-            "two") 
-                result=$(echo $result | sed "s/two/2/")
-                ;;
-            "six") 
-                result=$(echo $result | sed "s/six/6/")
-                ;;
-        esac
-
-        # Check four digit numbers.
-        four=${1:$i:4}
-        case $four in
-            "four") 
-                result=$(echo $result | sed "s/four/4/")
-                ;;
-            "five") 
-                result=$(echo $result | sed "s/five/5/")
-                ;;
-            "nine") 
-                result=$(echo $result | sed "s/nine/9/")
-                ;;
-        esac
-
-        # Check five digit numbers.
-        five=${1:$i:5}
-        case $five in
-            "three") 
-                result=$(echo $result | sed "s/three/3/")
-                ;;
-            "seven") 
-                result=$(echo $result | sed "s/seven/7/")
-                ;;
-            "eight") 
-                result=$(echo $result | sed "s/eight/8/")
-                ;;
-        esac
-    done
-
-    echo $result
-}
-
-
 sum=0
 while read line; do
-    translated=$(translate $line)
+    # We need avoid things like "oneighthree", "twone" not 
+    # getting substituted to "183", "21"
+    translated=$(echo $line | sed \
+        -e "s/one/o1e/g" \
+        -e "s/two/t2o/g" \
+        -e "s/three/t3e/g" \
+        -e "s/four/f4r/g" \
+        -e "s/five/f5e/g" \
+        -e "s/six/s6x/g" \
+        -e "s/seven/s7n/g" \
+        -e "s/eight/e8t/g" \
+        -e "s/nine/n9e/g")
 
     # Use Perl-style regex to find numbers (grep). 
     # Join the grep result to a single line (xargs).
@@ -66,8 +23,6 @@ while read line; do
     # Get first and last digit found.
     number=${var:0:1}${var: -1}
     (( sum += number ))
-
-    echo $number
 done
 
 echo Result: ${sum}
